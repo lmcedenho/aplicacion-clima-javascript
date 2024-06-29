d232ad28e6ce668bacb31063b953f1b7e0870b18
Daya y LuisM <dayana@Dayanas-MacBook-Pro.local>
2024-06-29 14:34:51 -0300

    hemos hecho una web de consulta de clima para el curso de js

---
+++ script.js
@@ -0,0 +1,52 @@
+const urlBase = `https://api.openweathermap.org/data/2.5/weather`
+const API_KEY = 'API-KEY'
+const diffkelvin = 273.15
+
+document.getElementById('searchButton').addEventListener('click', () => {
+    const city = document.getElementById('cityInput').value;
+    if (city) {
+        fetchWeather(city)
+    } else {
+        alert('Ingrese una ciudad Valida');
+    }
+});
+
+function fetchWeather(city) {
+    fetch(`${urlBase}?q=${city}&appid=${API_KEY}&lang=es`)
+    .then(data => data.json())
+    .then(data => showWeatherData(data))
+}
+
+function showWeatherData(data) {
+    const divResponseData = document.getElementById('responseData')
+    divResponseData.innerHTML = ''
+
+    const cityName = data.name
+    const countryName = data.sys.country
+    const temp = data.main.temp
+    const humidity = data.main.humidity
+    const description = data.weather[0].description
+    const icon = data.weather[0].icon
+
+    const cityInfo = document.createElement('h2')
+    cityInfo.textContent = `${cityName}, ${countryName}`
+
+    const tempInfo = document.createElement('p')
+    tempInfo.textContent = `La temperatura es ${Math.floor(temp-diffkelvin)}ºC`
+    
+    const humidityInfo = document.createElement('p')
+    humidityInfo.textContent = `La humedad es del ${humidity}%`
+
+    const incoInfo = document.createElement('img')
+    incoInfo.src = `https://openweathermap.org/img/wn/${icon}@2x.png`
+
+    const descriptionInfo = document.createElement('p')
+    descriptionInfo.textContent = `La descricion meteorologica es ${description}`
+
+    divResponseData.appendChild(cityInfo)
+    divResponseData.appendChild(tempInfo)
+    divResponseData.appendChild(humidityInfo)
+    divResponseData.appendChild(incoInfo)
+    divResponseData.appendChild(descriptionInfo)
+    
+}
